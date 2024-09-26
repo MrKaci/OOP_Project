c94569211a2c4c1aeecf860ac4f39dd573d95357
Arezki Ait kaci <aitkaci.arezki@gmail.com>
2024-09-27 00:14:53 +0200

    Calcul cout & Test

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -2,47 +2,80 @@
 from django.db import models
 
 
+##########################################################################################
 class Ville(models.Model):
     nom = models.CharField(max_length=100)
     code_postal = models.IntegerField()
     prix_m2 = models.IntegerField()
 
+    # Méthode Calcul cout
+    def costs(self):
+        return self.prix_m2
+
     def __str__(self):
         return f"{self.nom} {self.code_postal} {self.prix_m2}"
 
 
+##########################################################################################
 # Classe Abstraite
 class Local(models.Model):
     nom = models.CharField(max_length=100)
     ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
     surface = models.IntegerField()
 
+    # Méthode Calcul cout
+    def costs(self):
+        return self.surface
+
     class Meta:
         abstract = True
 
 
+##########################################################################################
 class SiegeSocial(Local):
     pass
 
 
+##########################################################################################
 class Machine(models.Model):
     nom = models.CharField(max_length=100)
-    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    prix = models.IntegerField()
     n_serie = models.CharField(max_length=100)
+
+    # Méthode Calcul cout
+    def costs(self):
+        return self.prix
 
     # Méthode str
     def __str__(self):
         return f"{self.nom} {self.prix} {self.n_serie}"
 
 
-class Usine(models.Model):
+##########################################################################################
+class Usine(Local):
     machines = models.ManyToManyField(Machine)
 
-    # Méthode str
-    def __str__(self):
-        return f"{self.machines}"
+    # Méthode Calcul cout
+    def costs(self):
+        TotCost = 0
+        CostMach = 0
+        CostRess = 0
+
+        for machine in self.machines.all():
+            CostMach = CostMach + machine.prix
+        print(CostMach)
+
+        for stock in self.stock_set.all():
+            CostRess += stock.nombre * stock.ressource.prix
+        print(CostRess)
+
+        print(self.ville.prix_m2 * self.surface)
+        TotCost = CostMach + CostRess + self.ville.prix_m2 * self.surface
+
+        return TotCost
 
 
+##########################################################################################
 #  Classe Abstraite
 class Objet(models.Model):
     nom = models.CharField(max_length=100)
@@ -52,19 +85,26 @@ class Objet(models.Model):
         abstract = True
 
 
+##########################################################################################
 class Ressource(Objet):
     pass
 
 
+##########################################################################################
 class QuantiteRessource(models.Model):
     ressource = models.ForeignKey(Ressource, on_delete=models.CASCADE)
     quantite = models.IntegerField()
+
+    # Méthode Calcul cout
+    def costs(self):
+        return self.quantite
 
     # Méthode str
     def __str__(self):
         return f"{self.ressource} {self.quantite}"
 
 
+##########################################################################################
 class Etape(models.Model):
     nom = models.CharField(max_length=100)
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
@@ -79,6 +119,7 @@ class Etape(models.Model):
         return f"{self.nom} {self.duree} {self.quantite_ressource} {self.etape_suivante} {self.machine}"
 
 
+##########################################################################################
 class Produit(Objet):
     premiere_etape = models.ForeignKey(Etape, on_delete=models.CASCADE)
 
@@ -87,9 +128,15 @@ class Produit(Objet):
         return f"{self.premiere_etape}"
 
 
+##########################################################################################
 class Stock(models.Model):
     ressource = models.ForeignKey(Ressource, on_delete=models.CASCADE)
     nombre = models.IntegerField()
+    usine = models.ForeignKey(Usine, on_delete=models.CASCADE)
+
+    # Méthode Calcul cout
+    def costs(self):
+        return self.nombre
 
     # Méthode str
     def __str__(self):
--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -1,11 +1,29 @@
 # Create your tests here.
 from django.test import TestCase
-from .models import Machine
+from .models import Machine, Usine, Ressource, Stock, Ville
 
 
 #   Test de modèle machine
-class MachineModelTests(TestCase):
-    def test_machine_creation(self):
-        self.assertEqual(Machine.objects.count(), 0)
-        Machine.objects.create(nom="scie", prix=1_000, n_serie="1683AI2")
-        self.assertEqual(Machine.objects.count(), 1)
+# class MachineModelTests(TestCase):
+#    def test_machine_creation(self):
+#        self.assertEqual(Machine.objects.count(), 0)
+#        Machine.objects.create(nom="scie", prix=1_000, n_serie="1683AI2")
+#        self.assertEqual(Machine.objects.count(), 1)
+
+
+#   Test de calcul de cout
+class CalculCoutTests(TestCase):
+    def test_calcul_cout(self):
+        V = Ville.objects.create(nom="Labège", code_postal=31400, prix_m2=2000)
+        U = Usine.objects.create(nom="Thanina Company", ville=V, surface=50)
+        m1 = Machine.objects.create(prix=1000)
+        m2 = Machine.objects.create(prix=2000)
+        U.machines.add(m1)
+        U.machines.add(m2)
+        r1 = Ressource.objects.create(nom="bois", prix=10)
+        Stock.objects.create(ressource=r1, nombre=1000, usine=U)
+        r2 = Ressource.objects.create(nom="mine", prix=15)
+        Stock.objects.create(ressource=r2, nombre=50, usine=U)
+
+        # a = Usine.objects.first()
+        print(U.costs())
