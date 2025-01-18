6f66bfe91e00af25b5d26bb66bb4defe6563c778
Arezki Ait kaci <aitkaci.arezki@gmail.com>
2025-01-18 12:17:47 +0100

    modifier views.py

--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.views.generic import DetailView
+from django.http import JsonResponse
 from .models import (
     Machine,
     Usine,
@@ -11,67 +12,80 @@ from .models import (
     Produit,
     SiegeSocial,
 )
-from django.http import JsonResponse
 
 
 class MachineDetailView(DetailView):
     model = Machine
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        machine = self.get_object()
+        data = {
+            "nom": machine.nom,
+            "prix": machine.prix,
+            "n_serie": machine.n_serie,
+        }
+        return JsonResponse(data)
 
 
 class UsineDetailView(DetailView):
     model = Usine
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        usine = self.get_object()
+        return JsonResponse(usine.json())
 
 
 class RessourceDetailView(DetailView):
     model = Ressource
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        ressource = self.get_object()
+        return JsonResponse(ressource.json())
 
 
 class StockDetailView(DetailView):
     model = Stock
 
-    def render_to_response(self, *arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        stock = self.get_object()
+        return JsonResponse(stock.json())
 
 
 class VilleDetailView(DetailView):
     model = Ville
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        ville = self.get_object()
+        return JsonResponse(ville.json())
 
 
 class QuantiteRessourceDetailView(DetailView):
     model = QuantiteRessource
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        quantite = self.get_object()
+        return JsonResponse(quantite.json())
 
 
 class EtapeDetailView(DetailView):
     model = Etape
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        etape = self.get_object()
+        return JsonResponse(etape.json())
 
 
 class ProduitDetailView(DetailView):
     model = Produit
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        produit = self.get_object()
+        return JsonResponse(produit.json())
 
 
 class SiegeSocialDetailView(DetailView):
     model = SiegeSocial
 
-    def render_to_response(self, arg, **response_kwargs):
-        return JsonResponse(self.object.json())
+    def get(self, request, *args, **kwargs):
+        siege = self.get_object()
+        return JsonResponse(siege.json())
